7849942af30c39871a4d36d5ee87990a4558594d
victoryokirie <victoryokirie98@gmail.com>
2022-07-27 15:05:18 +0100

    linear search for objects

--- search algorithms/linear search.js
+++ search algorithms/linear search.js
@@ -59,3 +59,32 @@ function findNumber(arr, elementToBeFound){
 const array = [4,6,2,7,1,8,40,32,20,21]
 
 findNumber(arr, 20)
+
+
+// These will not work for an array of objects. it works well for primitive data types(strings, numbers)
+
+// For objects one has to tweak the algorithm a bit to ensure the datatype is an object
+
+function findEmployee(employees, employeeDetails){
+
+    let index =0
+    for (const item of employees){
+       if(typeof employeeDetails === 'object' && employeeDetails !==null && employeeDetails.name === item.name) {
+           return index
+       }
+
+   
+        index++
+    }
+    
+
+}
+
+const employees=[
+    {name: 'Tony', Age:26},
+    {name: 'Jane', Age:23},
+    {name: 'Ben', Age:21},
+    {name: 'Mike', Age:25}
+]
+
+console.log(findEmployee(employees,{name: 'Ben', Age:21}))
