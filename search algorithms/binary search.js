df8a82d5b16cd0d8b855f02a3fef4ffc4f47e981
victoryokirie <victoryokirie98@gmail.com>
2022-07-27 17:57:43 +0100

    second commit, binary search

---
+++ search algorithms/binary search.js
@@ -0,0 +1,27 @@
+//Binary search only works on ordered lists. 
+
+function findElement(sortedArr, element){
+    let startIndex = 0
+    let endIndex = sortedArr.length - 1
+
+    while(startIndex <= endIndex){
+        const middleIndex =startIndex + Math.floor((endIndex - startIndex ) / 2)
+
+        if(element === sortedArr[middleIndex] ){
+            return middleIndex
+        }
+
+        if(sortedArr[middleIndex] < element){
+            startIndex = middleIndex + 1
+        }else{
+            endIndex = middleIndex -1
+        }
+    }
+    
+}
+
+
+const arr =[1,3,5,7,9,11,13,15,17,19,21]
+
+console.log(findElement(arr, 3))
+console.log(findElement(arr, 17))
